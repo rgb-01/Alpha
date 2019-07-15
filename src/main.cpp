eccb39ee316310b12aea47f725f66e9c5d256450
Ma'ruf Shidiq <maruf.shidiq@mail.ugm.ac.id>
2019-07-16 06:20:27 +0700

    Update delay time

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,11 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
 
   // wait for a second
-  delay(1000);
+  delay(500);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
 
   // wait for a second
-  delay(1000);
+  delay(500);
 }
